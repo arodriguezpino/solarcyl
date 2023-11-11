74a1d971bc8f678d851b2ab51f898097f2a60162
Alvaro Rodriguez Pino <arodriguezpino@serbatic.es>
2023-11-11 13:08:17 +0100

    Updated workflow

--- solarcyl-app/vite.config.ts
+++ solarcyl-app/vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/solarcyl-app/'
+  base: 'https://arodriguezpino.github.io/solarcyl'
 })

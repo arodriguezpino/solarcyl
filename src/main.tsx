4d97e3d9193a753de879f65357e0acc2692a37a3
Alvaro Rodriguez Pino <arodriguezpino@serbatic.es>
2023-11-13 22:18:49 +0100

    Other fix

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,9 @@
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import {
+  createHashRouter,
+  RouterProvider,
+} from "react-router-dom";
 import Layout from "./components/layout/LayoutApp";
 import MapPage from "./components/pages/Map/Map.page";
 import EvolutionPage from "./components/pages/Evolution/Evolution.page";
@@ -8,7 +11,7 @@ import CalculatorPage from "./components/pages/Calulator/Calculator.page";
 import NewsPage from "./components/pages/News/News.page";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-const router = createBrowserRouter([
+const router = createHashRouter([
   {
     path: "/",
     element: <Layout />,
